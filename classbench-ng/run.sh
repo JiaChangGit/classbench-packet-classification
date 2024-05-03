3a8364bdda1e9c533f4c5db4a286b4527e7b8e3e
Jia <jiachanggit@gmail.com>
2024-05-03 11:11:48 +0800

    Upd run.sh

--- classbench-ng/run.sh
+++ classbench-ng/run.sh
@@ -1,4 +1,4 @@
-# ex: $1 -> acl1,  $2-> 100000
-# sh run.sh acl1 90000
+# ex: $1 -> acl1,  $2-> 10000
+# sh run.sh acl1 10000
 ulimit -s 81920
 ./classbench generate v4 ./vendor/parameter_files/$1_seed --count=$2 --db-generator=./vendor/db_generator/db_generator > $1_$2.txt
